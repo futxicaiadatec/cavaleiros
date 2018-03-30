a68e2aa752c87634a8108e14d75318bf557504b0
degumes <degumes@users.noreply.github.com>
2018-03-29 23:17:12 -0400

    highlight

---
+++ 0.js
@@ -0,0 +1,11 @@
+/**
+* 0.0
+* existe uma variável global "zero" já declarada
+*/
+var zero = "000000000"
+
+/**
+* 0.1
+* encontre o console
+*/
+console.log("%cACHOU!", "font-size: large; color: red; font-weight: bold;")
